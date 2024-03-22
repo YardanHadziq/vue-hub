07de2a8038425534f95b8543f29236366e40c1e1
YardanHadziq <yardanbusiness@gmail.com>
2024-03-22 10:45:18 +0700

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!--h1>
+    Hello Kelas F
+  </h1-->
+  <Texth />
+
+  <br>
+
+  <buttonc />
+</template>
+
+<script setup>
+  import buttonc from "./components/buttonc.vue"
+  import texth from "./components/TextHeader.vue"
+
+</script>
